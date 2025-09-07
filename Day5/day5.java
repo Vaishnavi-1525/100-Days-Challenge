6d2c30f98bda95764f53bd8ed9ca64744440cca6
Vaishnavi Arjun Kanade <vaishnavikanade1525@gmail.com>
2025-09-07 21:39:04 +0530

    day5.java

---
+++ Day5/day5.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class day5
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter a Number");
+		int ch=s.nextInt(); 
+		
+		 // Check if it is Even Number 
+		if( ch %2==0 )
+		{
+			System.out.println(ch + " is an Even Number");
+		}
+		 // If the input is a Odd Number
+		else
+		{
+			System.out.println(ch + " is an Odd Number");
+		}
+		s.close();
+	}
+}
