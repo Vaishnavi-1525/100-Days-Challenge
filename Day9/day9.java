54ea2be441de3483e7d30327a8521fa9b0ac4d08
Vaishnavi Arjun Kanade <vaishnavikanade1525@gmail.com>
2025-09-09 19:12:03 +0530

    day9.java

---
+++ Day9/day9.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Day9 
+{
+
+    public static void main(String[] args)
+  {
+        // Create Scanner object to take user input
+        Scanner sc = new Scanner(System.in);
+
+        // Ask user for a number
+        System.out.print("Enter a Number: ");
+        int num = sc.nextInt();
+
+        // Temporary variable to process number
+        int temp = num;
+
+        // Counter variable to count digits
+        int count = 0;
+
+        // Special case: if number is 0, it has 1 digit
+        if (temp == 0) {
+            count = 1;
+        } else {
+            // Loop until number becomes 0
+            while (temp > 0) {
+                temp = temp / 10;  // Remove last digit
+                count++;           // Increase digit count
+            }
+        }
+
+        // Print result
+        System.out.println("Number of Digits: " + count);
+
+        // Close scanner (good practice)
+        sc.close();
+    }
+}
