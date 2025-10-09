32e0c796ec734e7ec215cb7ca5429dc2ab4e238f
Vaishnavi Arjun Kanade <vaishnavikanade1525@gmail.com>
2025-10-09 19:09:34 +0530

    day56.java

---
+++ Day56/day56.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ReplaceSubstring 
+{
+    public static void main(String[] args) 
+  {
+        Scanner sc = new Scanner(System.in);
+
+        //  Input original string
+        System.out.print("Enter the original string: ");
+        String original = sc.nextLine();
+
+        //  Input substring to be replaced
+        System.out.print("Enter the substring to replace: ");
+        String oldSub = sc.nextLine();
+
+        //  Input new substring
+        System.out.print("Enter the new substring: ");
+        String newSub = sc.nextLine();
+
+        //  Replace substring using replace() method
+        String modified = original.replace(oldSub, newSub);
+
+        //  Output the final string
+        System.out.println("Modified string: " + modified);
+
+        sc.close();
+    }
+}
