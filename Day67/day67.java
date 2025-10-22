a05e69f31c10f7bb3632316ad0a95c21a2e62468
Vaishnavi Arjun Kanade <vaishnavikanade1525@gmail.com>
2025-10-22 18:32:26 +0530

    day67.java

---
+++ Day67/day67.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class day67 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input size
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Reverse the array
+        System.out.println("Reversed array:");
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
