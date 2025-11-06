647bf49d332cb0994700926f94cf1b40a7d3e5cc
Vaishnavi Arjun Kanade <vaishnavikanade1525@gmail.com>
2025-11-06 19:08:51 +0530

    day86.java

---
+++ Day86/day86.java
@@ -0,0 +1,27 @@
+
+class Triangle {
+    void triangleType() {
+        System.out.println("I am a triangle");
+    }
+}
+
+class Isosceles extends Triangle {
+    void isoscelesType() {
+        System.out.println("I am an isosceles triangle");
+    }
+}
+
+class Equilateral extends Isosceles {
+    void equilateralType() {
+        System.out.println("I am an equilateral triangle");
+    }
+}
+
+public class day86 {
+    public static void main(String[] args) {
+        Equilateral eq = new Equilateral();
+        eq.equilateralType();
+        eq.isoscelesType();
+        eq.triangleType();
+    }
+}
